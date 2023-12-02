980dd310e8ccb90496558010a0472b32846ef55f
yuyinws <lyc657508366@gmail.com>
2023-12-02 22:12:01 +0800

    update

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import TurboConsole from 'vite-plugin-turbo-console'
+import TurboConsole from 'unplugin-turbo-console/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
